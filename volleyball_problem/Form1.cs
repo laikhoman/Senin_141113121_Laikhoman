8296d9624a41b48d2ea221d25da23aaff39f896f
laikhoman <luxkoman@yahoo.com>
2016-11-26 16:51:01 +0700

    Revised volleyball_problem

--- volleyball_problem/Form1.cs
+++ volleyball_problem/Form1.cs
@@ -16,54 +16,15 @@ namespace volleyball_problem
         {
             InitializeComponent();
         }
-        public static long fakto(long n)
+        public static long gcd(long a, long b)
         {
-            if (n == 1 || n == 0)
-                return 1;
-            return fakto(n - 1) * n;
+            if (b == 0) return a;
+            return gcd(b, a % b);
         }
-        /*
         public static long modInverse(long a, long n)
         {
             long i = n, v = 0, d = 1;
-            while (a > 0)
-            {
-                long t = i / a, x = a;
-                a = i % x;
-                i = x;
-                x = d;
-                d = v - t * x;
-                v = x;
-            }
-            v %= n;
-            if (v < 0) v = (v + n) % n;
-            return v;
-        }
-        */
-        public static long modPow(long a, long x, long p)
-        {
-            // return a^x mod p
-            long res = 1;
-            while (x > 0)
-            {   
-                if ((x % 2) == 1) 
-                    res = (res * a) % p;
-                a = (a * a) % p;
-                x /= 2;
-            }
-            return res%p;
-        }
-        /*
-        public static long modInverse(long a, long p)
-        {
-            // return modular multiplicative of: a mod p, assuming p is prime
-            return modPow(a, p - 2, p)%p;
-        }
-        */
-        
-        public static long modInverse(long a, long n)
-        {
-            long i = n, v = 0, d = 1;
+            if (n <= 1 || gcd(a, n) != 1) return -1;
             while (a > 0)
             {
                 long t = i / a, x = a;
@@ -75,7 +36,7 @@ namespace volleyball_problem
             }
             if (v < 0) v = (v + n) % n;
             return v;
-        }
+        } 
         public static long modBinomial(long n, long k, long p)
         {
             // return C(n,k) mod p, assuming p is prime
@@ -83,41 +44,17 @@ namespace volleyball_problem
             int i;
             for (i = 0; i < k; i++)
             {
-                numerator *= (n - i);
+                numerator *= (n-i);
                 numerator %= p;
             }
-            long denominator = 1; // k!
-            for (i = 2; i <= k; i++)
+            long denominator1 = 1; // k!
+            for (long j = 1; j <= k; j++)
             {
-                denominator *= modInverse(i, p);
-                denominator %= p;
+                denominator1 *= modInverse(j,p);
+                denominator1 %= p;
             }
-            //long res = modInverse(denominator, p);
-            //denominator %= p;
-            long res = (denominator * numerator) % p;
-            return res;
-        }
-        public static long combNmodulus(long n, long r, long p)
-        {
-            long result = 1;
-
-            for (long idx1 = n - r + 1; idx1 <= n; idx1++)
-            {
-                result = (result * idx1)%p;
-                if (result > p)
-                    result = result % p;
-            }
-            
-            for (long idx2 = 2; idx2 <= r;idx2++ )
-            {
-                result = (result * idx2) % p;
-                long invers = modInverse(idx2, p);
-                result = (result * invers) % p;
-                if (result > p)
-                    result = result % p;
-            }
-            
-            return result;
+            long res = (denominator1 * numerator) % p;
+            return res%p;
         }
         
         private void BtnHitung_Click(object sender, EventArgs e)
@@ -128,19 +65,35 @@ namespace volleyball_problem
             
             if ((a >= 0 && a <= 1000000000) && (b >= 0 && b <= 1000000000))
             {
-                if (((a == 25 && b < 24) || (b == 25 && a < 24)) || ((a >= 24 && b >= 24) && System.Math.Abs(a - b) == 2))
+                if ((a == 25 && b < 24) || (b == 25 && a < 24))
                 {
                     long sum = a + b - 1;
                     if (a > b)
                     {
-                        //combi = combNmodulus(sum, b, 1000000007);
                         combi = modBinomial(sum, b, 1000000007);
+
                     }
                     else
                     {
-                        //combi = combNmodulus(sum, a, 1000000007);
                         combi = modBinomial(sum, a, 1000000007);
                     }
+                }
+                else if ((a >= 24 && b >= 24) && System.Math.Abs(a - b) == 2)
+                {
+                    long combi24 = modBinomial(48, 24, 1000000007);
+                    long sum = a + b;
+                    if (a > b)
+                    {
+                        combi = modBinomial(sum-48-2, b-24, 1000000007);
+                        combi *= combi24;
+                    }
+                    else
+                    {
+                        combi = modBinomial(sum-48-2, a-24, 1000000007);
+                        combi *= combi24;
+                    }
+                    if (combi > 1000000007)
+                        combi %= 1000000007;
                 }
                 else
                 {
